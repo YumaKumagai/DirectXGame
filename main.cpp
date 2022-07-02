cde06912a573d210e330f1aa11b3a3e1ca605eeb
YumaKumagai <k021g1441@g.neec.ac.jp>
2022-07-03 02:30:26 +0900

    ウィンドウタイトルを自分の名前に変更

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("GE2B_09_クマガイ_ユウマ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
